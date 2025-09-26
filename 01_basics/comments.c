5c5a3a10311eaa1a59a9c9c1955050cbcdf56edc
M.J lali <diyalali49@gmail.com>
2025-09-26 07:12:43 +0000

    new changes done

--- 01_basics/comments.c
+++ 01_basics/comments.c
@@ -1,5 +1,15 @@
 #include<stdio.h>
 void main()
 {
+     // This is a single-line comment
+    printf("Hello, this is a single-line comment example!\n");
+
+    /*
+      This is a multi-line comment.
+      You can write more than one line here.
+      The compiler will ignore everything between
+      the opening   and closing  comment symbols.
+      */
     
+    printf("This is a multi-line comment example!\n");
 }
---
+++ 01_basics/variables.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+void main()
+{
+     // variable is a quantity that can be during program execution
+
+   //Variable Declaration  ** synatax  data_type variable_name;
+  
+    
+ float average;
+ double salary;
+   // for many variables with one data type
+ char ch ,grade;
+ int marks,number,age;
+
+
+   //Variable Initialization ** syntax  data_type variable_name = value;
+ int n = 29;
+//  char ch = 'A';
+//  float average = 4.5;
+//  double salary = 8000000000000.42;
+                
+printf("your numbers are %d\n",n);
+
+   
+}
