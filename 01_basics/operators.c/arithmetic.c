b06bda2a92d4cfec531aaa0af458165c3b03b5c0
M.J lali <diyalali49@gmail.com>
2025-09-27 05:31:33 +0000

    arithmetic done

---
+++ 01_basics/operators.c/arithmetic.c
@@ -0,0 +1,27 @@
+#include<stdio.h>
+void main()
+{
+    
+    int a,b;
+    printf("Enter a number in a:\n");
+     scanf("%d",&a);
+    printf("Enter a number in b:\n");
+    scanf("%d",&b);
+
+    printf("a + b = %d\n",a + b);       //addition
+    printf("a - b = %d\n",a - b);         //subtraction
+    printf("a * b = %d\n",a * b);         //multiplication
+    printf("a / b = %d\n",a / b);        //division
+    printf("a %% b = %d\n",a % b);         // modulus
+      
+    //  unary operators
+    // --a;
+    // +b;
+    // !a;
+    // binary operators
+    // a+b;
+    // a>b;
+    // a=b; 
+
+
+}
