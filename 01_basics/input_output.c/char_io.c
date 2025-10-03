17fc08d08088ef08414bb2dbd3e4c3d99f8ac7fd
M.J lali <diyalali49@gmail.com>
2025-10-03 06:50:39 +0000

    character I/O done

---
+++ 01_basics/input_output.c/char_io.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+void main()
+{
+    char ch;
+
+    printf("Enter a character:");  // to show a request  as output
+    scanf("%c",&ch);              //to input a character
+
+    printf("You entered %c ",ch);
+}
