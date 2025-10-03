5ac3675775f0a5fdb80147be30e49b562312d5d8
M.J lali <diyalali49@gmail.com>
2025-10-03 07:26:46 +0000

    hello world done

--- 01_basics/hello_world.c
+++ 01_basics/hello_world.c
@@ -7,32 +7,3 @@ void main()
 
      printf("I am kneely interested in programming\n");
 }
-/*01_basics/
-├─ hello_world.c
-├─ comments.c
-├─ variables.c
-├─ data_types.c
-├─ constants.c
-├─ operators.c
-│   ├─ arithmetic_operators.c
-│   ├─ relational_operators.c
-│   ├─ logical_operators.c
-│   ├─ assignment_operators.c
-│   └─ increment_decrement.c
-├─ input_output.c
-│   ├─ char_input_output.c
-│   ├─ string_input_output.c
-│   ├─ formatted_output.c
-│   └─ file_input_output.c
-├─ control_statements.c
-│   ├─ if_else.c
-│   ├─ switch_case.c
-│   └─ loops.c
-│       ├─ for_loop.c
-│       ├─ while_loop.c
-│       └─ do_while_loop.c
-└─ functions.c
-    ├─ simple_function.c
-    ├─ function_with_parameters.c
-    └─ function_with_return.c
-*/
