8d7b075d1c05b8a3e86aee3b9d386f281ef8e003
M.J lali <diyalali49@gmail.com>
2025-10-05 07:41:23 +0000

    if done

---
+++ 02_conditions/if.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+void main()
+{
+    int marks;
+    printf("Enter your marks:");
+    scanf("%d",&marks);
+    if(marks>=40)
+    {
+        printf("Congratulations, You have passed.\n");
+    }
+}
