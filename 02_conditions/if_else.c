4dc403eda51a343019989d6d06a76a6082dfe6dd
M.J lali <diyalali49@gmail.com>
2025-10-13 01:26:09 +0000

    if_else done

---
+++ 02_conditions/if_else.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+void main()
+{
+    int y;
+    printf("Enter a year:");
+    scanf("%d",&y);
+    if(y%4==0)
+    printf("%d is a leap year.\n",y);
+    else
+    printf("%d is not a leap year\n.",y);
+
+}
