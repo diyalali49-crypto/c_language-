9086fe01012cc44ddd3e49a30018b407f84bc123
M.J lali <diyalali49@gmail.com>
2026-01-25 05:57:16 +0000

    sentinel_con.-loop done

---
+++ 03_loops/sentinel-controlled-loop.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{
+    int n;
+    n=1;
+    while(n!= -1)            //-1 is the special value known as sentinel value.
+    {
+        printf("Enter a numner:");
+        scanf("%d",&n);
+        printf("you entered %d\n",n);
+    }
+    printf("End of program........");
+    return 0;
+}
+// gcc 03_loops/sentinel-controlled-loop.c -o sentinel-controlled-loop.exe  && ./sentinel-controlled-loop.exe
