235271ed851567ade0541c9ca3d0b804293acdfc
M.J lali <diyalali49@gmail.com>
2026-02-07 08:19:17 +0000

    return value thriugh assignment statement

--- 04_Funtions/main.c
+++ 04_Funtions/main.c
@@ -1,22 +1,24 @@
 #include <stdio.h>
-int add(int a.int b);
-void cube(int num);
+int add(int a,int b);
+int cube(int num);
 
 void main()
-
-
-{
-    add(2.3);
-    cube(5);
+{  
+    int x,y;
+       x= add(2,3);
+       y= cube(5);
+       printf("cube is %d\n",y);
+        printf("Adding numbers is %d\n",x);
 }
 
-int add(int a,int b)
-{
-    return a+b;
+ int add(int a,int b)
+    {
+           return a+b;
+     }
 
-}
- void cube(int num)
+ int cube(int num)
  {
     return num*num*num;
- }
+  }
+
  // gcc 03_loops/sentinel-controlled-loop.c -o sentinel-controlled-loop.exe  && ./sentinel-controlled-loop.exe
