ca98505467751cf308a1527e69eab5cc98e089e9
M.J lali <diyalali49@gmail.com>
2026-02-10 08:41:32 +0000

    number checking done

---
+++ 04_Functions/even-odd.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+ int IsEven(int n)
+ {
+    if (n%2==0)
+       return 1;
+    else
+       return 0;
+ }
+  int main()
+ {  int num;
+    printf("Enter a number:");
+    scanf("%d",&num);
+    if(IsEven(num))
+    printf("%d is a Even number\n",num);
+    else
+    printf("%d is odd number\n",num);
+return 0;
+ }
